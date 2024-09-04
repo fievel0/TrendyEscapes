6a4a573b006e204afc1401a73339b7a8f02de1e3
Julian Peña <julianpr8@hotmail.com>
2024-09-03 22:46:38 -0500

    Dependencias arregladas. Flyway y conexión a PostgreSQL configurado. Primeras migraciones de la db. Agregar carpeta entidades y clase SpringBootApplication.

--- Backend/target/classes/db/migration/V2__autentication_migration.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Autenticacion (
-    Id_Autenticacion INT PRIMARY KEY IDENTITY,
-    Id_C INT,
-    UserName NVARCHAR(100),
-    Password NVARCHAR(100)
-);
-
-ALTER TABLE Autenticacion
-ADD FOREIGN KEY (Id_C) REFERENCES Cliente(Id_C);
