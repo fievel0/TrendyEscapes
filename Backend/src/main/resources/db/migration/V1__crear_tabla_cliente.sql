6cc3b377fa73b278b70684c28b0e69007ab59d6c
Julian Peña <julianpr8@hotmail.com>
2024-09-05 00:20:55 -0500

    Migraciones actualizadas. Cambio en nombre de columnas.

--- Backend/src/main/resources/db/migration/V1__crear_tabla_cliente.sql
+++ Backend/src/main/resources/db/migration/V1__crear_tabla_cliente.sql
@@ -1,16 +1,13 @@
 CREATE TABLE Cliente (
-    id_c bigserial,
-    id_tipodocumento bigint,
+    id_cliente bigserial,
     id_pais bigint,
     id_formapago bigint,
     id_autenticacion bigint,
     nombre VARCHAR(100),
     num_documento VARCHAR(50),
     fecha_nacimiento date,
-    edad INT,
     direccion VARCHAR(255),
     telefono VARCHAR(50),
-    email VARCHAR(100),
     PRIMARY KEY(id_c)
 );
 
