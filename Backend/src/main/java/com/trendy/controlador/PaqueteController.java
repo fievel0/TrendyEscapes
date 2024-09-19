8e565cbd37295590ed32220ec3dc14f27c3290ce
Julian Peña <julianpr8@hotmail.com>
2024-09-19 16:43:16 -0500

    Docker file agregado

--- Backend/src/main/java/com/trendy/controlador/PaqueteController.java
+++ Backend/src/main/java/com/trendy/controlador/PaqueteController.java
@@ -1,7 +1,6 @@
 package com.trendy.controlador;
 
-<<<<<<< HEAD
-=======
+
 import com.trendy.entidades.dtos.paquete.PaqueteDTO;
 import com.trendy.servicio.paquete.PaqueteService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,7 +12,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
->>>>>>> 945083c139d700af95e5bf59cd15622eebf0a00e
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,11 +19,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.trendy.entidades.dtos.paquete.PaqueteDTO;
 import com.trendy.servicio.paquete.PaqueteService;
